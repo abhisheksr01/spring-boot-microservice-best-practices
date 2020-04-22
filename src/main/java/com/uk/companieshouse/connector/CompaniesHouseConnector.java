a195d11eb222eecf76bf53a27b436150e95275f2
abhisheksr01 <abhisheksingh2691@gmail.com>
2020-04-22 14:50:55 +0100

    fix checkstyle

--- src/main/java/com/uk/companieshouse/connector/CompaniesHouseConnector.java
+++ src/main/java/com/uk/companieshouse/connector/CompaniesHouseConnector.java
@@ -2,7 +2,6 @@ package com.uk.companieshouse.connector;
 
 import com.uk.companieshouse.model.CompaniesHouseGovUKResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
