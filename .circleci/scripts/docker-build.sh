b31879286ce56814b47ff3b31661d433d9f3aac6
abhisheksr01 <abhisheksingh2691@gmail.com>
2020-05-21 15:59:12 +0100

    feat:CircleCI: Add buildAndPushImage and checkImageVulnerability jobs in pipeline

---
+++ .circleci/scripts/docker-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+TAG="0.1.${CIRCLE_BUILD_NUM}"
+echo "${TAG}" > version.txt
+
+echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
+docker build -t "${DOCKER_USER}/${PRODUCT}:${TAG}" -f ci.Dockerfile .
+docker tag "${DOCKER_USER}/${PRODUCT}:${TAG}" "${DOCKER_USER}/${PRODUCT}:latest"
+docker push "${DOCKER_USER}/${PRODUCT}:${TAG}"
+docker push "${DOCKER_USER}/${PRODUCT}:latest"
