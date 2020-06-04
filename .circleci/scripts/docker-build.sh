dfe5d5ec04de5f5988823e8cf62e80882c863ec4
abhisheksr01 <abhisheksingh2691@gmail.com>
2020-06-04 18:23:52 +0100

    Added circleci build for dependabot branches and upgraded gradlew to 6.5

--- .circleci/scripts/docker-build.sh
+++ .circleci/scripts/docker-build.sh
@@ -2,11 +2,13 @@
 
 set -euo pipefail
 
-TAG="0.1.${CIRCLE_BUILD_NUM}"
+TAG=$([ "${CIRCLE_BRANCH}" == "master" ] && echo "0.1.${CIRCLE_BUILD_NUM}" || echo "${CIRCLE_BRANCH}")
 echo "${TAG}" >version.txt
 echo "New Docker Image Version : ${TAG}"
 echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
 docker build -t "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}" -f ci.Dockerfile .
-docker tag "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}" "${DOCKER_USER}/${DOCKER_IMAGE}:latest"
 docker push "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}"
-docker push "${DOCKER_USER}/${DOCKER_IMAGE}:latest"
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
+  docker tag "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}" "${DOCKER_USER}/${DOCKER_IMAGE}:latest"
+  docker push "${DOCKER_USER}/${DOCKER_IMAGE}:latest"
+fi
