98d4c43950ef09e17fbc7d0eaf5f9644d241b036
abhisheksr01 <abhisheksingh2691@gmail.com>
2020-06-13 16:35:29 +0100

    fix(CucumberTest.java): fix failing test due to dependency upgradation

--- src/test/java/com/uk/companieshouse/e2e/CucumberTest.java
+++ src/test/java/com/uk/companieshouse/e2e/CucumberTest.java
@@ -1,13 +1,23 @@
 package com.uk.companieshouse.e2e;
 
+import com.uk.companieshouse.CompaniesHouseApplication;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(Cucumber.class)
+@CucumberContextConfiguration
+@SpringBootTest(
+        classes = CompaniesHouseApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 @CucumberOptions(features = "src/test/resources",
         plugin = {
-                "pretty", "html:build/reports/cucumber", "json:build/reports/cucumber/cucumber.json"
+                "pretty", "html:build/reports/cucumber", "json:build/reports/cucumber/"
         })
 public class CucumberTest {
+//        TODO: Fix Cucumber report not generating issue, the plugin is not accepting the params properly
 }
--- src/test/java/com/uk/companieshouse/e2e/SpringIntegration.java
+++ src/test/java/com/uk/companieshouse/e2e/SpringIntegration.java
@@ -1,14 +1,7 @@
 package com.uk.companieshouse.e2e;
 
-import com.uk.companieshouse.CompaniesHouseApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest(
-        classes = CompaniesHouseApplication.class,
-        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@ActiveProfiles("test")
 public abstract class SpringIntegration implements com.uk.companieshouse.e2e.WireMockService {
     protected static final String DEFAULT_URL = "http://localhost:8081/";
     protected RestTemplate restTemplate = new RestTemplate();
