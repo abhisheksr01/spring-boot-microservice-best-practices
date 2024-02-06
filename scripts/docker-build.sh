43b468bad8cb9e6f04472bd10ed56e72e836e68a
abhisheksr01 <abhisheksingh2691@gmail.com>
2024-02-06 14:18:50 +0000

    build(#287): rename default master branch to main

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -10,7 +10,7 @@ lint_docker_file() {
 
 build_image() {
   printMessage "$HEART_ICON" "$HEART_ICON" "Start Building Docker Image..."
-  TAG=$([ "${CIRCLE_BRANCH}" == "master" ] && echo "0.1.${CIRCLE_BUILD_NUM}" || echo "${CIRCLE_BRANCH}" | sed 's/dependabot\/gradle//g;s/.//')
+  TAG=$([ "${CIRCLE_BRANCH}" == "main" ] && echo "0.1.${CIRCLE_BUILD_NUM}" || echo "${CIRCLE_BRANCH}" | sed 's/dependabot\/gradle//g;s/.//')
   echo "${TAG}" > version/docker-version.txt
   echo "${TAG}" > version/test.txt
   printMessage "$HEART_ICON" "$HEART_ICON" "New Docker Image Version : ${TAG}"
@@ -21,14 +21,14 @@ push_image() {
   TAG=$(cat version/docker-version.txt)
   printMessage "$HEART_ICON" "$HEART_ICON" "print value from test.tx"
   cat version/test.txt
-  if [ "${CIRCLE_BRANCH}" == "master" ]; then
+  if [ "${CIRCLE_BRANCH}" == "main" ]; then
     printMessage "$HEART_ICON" "$HEART_ICON" "Pushing Docker Image latest & ${TAG} ..."
     docker tag "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}" "${DOCKER_USER}/${DOCKER_IMAGE}:latest"
     docker push "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}"
     docker push "${DOCKER_USER}/${DOCKER_IMAGE}:latest"
     printEmojiWrappedMessage "${GREEN}${HEART_ICON}" "Successfully pushed Docker Image '${DOCKER_USER}/${DOCKER_IMAGE}' with latest & ${TAG} tags..."
   else
-    printEmojiWrappedMessage "${GREEN}${HEART_ICON}" "Skipping push docker image for non master branch."
+    printEmojiWrappedMessage "${GREEN}${HEART_ICON}" "Skipping push docker image for non main branch."
   fi
 }
 "$@"
